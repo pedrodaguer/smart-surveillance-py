4514b8c37c8938cf4a6331e017feb65a89f33718
Pedro Daguer <pedrodaguer.dev@gmail.com>
2025-10-22 12:23:14 +0100

    feat(surveillance): implement video recording on movement detection

--- surveillance.py
+++ surveillance.py
@@ -1,11 +1,16 @@
+import datetime
 import time
 import cv2
 import winsound
+import os
 
 cam = cv2.VideoCapture(0)
 backSub = cv2.createBackgroundSubtractorMOG2()
 last_beep = 0
-delay_frames = 1
+delay_frames = 10
+recording = False
+out = None
+output_dir = ""
 
 if not cam.isOpened():
     print("Erro ao abrir a camera")
@@ -37,10 +42,32 @@ while cam.isOpened():
         x, y, w, h = cv2.boundingRect(cnt)
         frame_out = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 200), 3)
 
-    if large_contours and (time.time() - last_beep > 0.75) and delay_frames <= 0:
-        winsound.Beep(1000, 75)
-        last_beep = time.time()
+    if large_contours and delay_frames <= 0:
+        now = time.time()
 
+        if not recording:
+            output_dir = os.path.join("Alertas", datetime.date.today().strftime("%Y-%m-%d"))
+            os.makedirs(output_dir, exist_ok=True)
+
+            formatted_now = time.strftime("%Y-%m-%d_%H-%M-%S")
+            frame_height, frame_width = frame.shape[:2]
+
+            output_file = os.path.join(output_dir, f"movimento_{formatted_now}.mp4")
+            out = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*"mp4v"), 30, (frame_width, frame_height))
+
+            recording = True
+
+        if now - last_beep > 0.75:
+            winsound.Beep(1000, 75)
+            last_beep = now
+
+        out.write(frame)
+
+    else:
+        if recording:
+            recording = False
+            out.release()
+            out = None
 
     cv2.imshow('Live Feed', frame_out)
 
