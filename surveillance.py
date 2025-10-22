0a06938d18040cdac93348f9c9a873a64191f5b8
Pedro Daguer <pedrodaguer.dev@gmail.com>
2025-10-22 10:29:25 +0100

    feat(surveillance): enhance contour detection with thresholding and morphological operations

--- surveillance.py
+++ surveillance.py
@@ -21,13 +21,23 @@ while cam.isOpened():
         fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    frame_ct = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+    retval, mask_thresh = cv2.threshold(fg_mask, 180, 255, cv2.THRESH_BINARY)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+    mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
+
+    min_contour_area = 500
+    large_contours = [
+        cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area
+    ]
+
+    frame_ct = cv2.drawContours(frame, large_contours, -1, (0, 255, 0), 2)
 
     cv2.imshow("Frame", frame_ct)
 
     if cv2.waitKey(1) == 27:
         break
-
 
 cam.release()
 cv2.destroyAllWindows()
